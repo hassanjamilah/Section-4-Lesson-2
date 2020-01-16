67c4507a217b2bd7ba8ee09d1915572ab17aafd6
Hassan Jamila <hassan.jamila@n2nconnect.com>
2020-01-16 21:32:43 +0800

    Initial Commit

---
+++ Section 4 Lesson 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Section 4 Lesson 2
+//
+//  Created by user on 16/01/2020.
+//  Copyright © 2020 Andalus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
